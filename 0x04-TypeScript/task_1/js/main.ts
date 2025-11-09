40afdd456464be8a2939a1b9cc5653166f0eea1d
David <davidtanimowo01@gmail.com>
2025-11-09 10:26:41 +0100

    fix: main.js

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,36 +1,37 @@
 // Define the Teacher interface
 interface Teacher {
-    firstName: string;
-    lastName: string;
-    fullTimeEmployee: boolean;
-    location: string;
-    yearsOfExperience?: number; // optional property
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number; // optional property
+  [key: string]: any; // allows any extra property
 }
 
 // Extend the Teacher interface with Directors
 interface Directors extends Teacher {
-    numberOfReports: number;
+  numberOfReports: number;
 }
 
 // Example usage
 const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
 
 // console.log(director1);
 
 // Define the function interface
 export interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+  (firstName: string, lastName: string): string;
 }
 
 // Implement the function
 export const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    return `${firstName.charAt(0)}. ${lastName}`;
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
 
 // Example usage
